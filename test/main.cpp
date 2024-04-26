0bea49ba861a0fad0278dafdd65d2c7061db7b27
Kevin Li <oathdruid@live.cn>
2024-04-26 17:47:51 +0800

    add ci.yml

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,11 @@
 #include "gtest/gtest.h"
 
+#ifdef ABSL_HAVE_ADDRESS_SANITIZER
+extern "C" const char* __asan_default_options() {
+    return "detect_odr_violation=1";
+}
+#endif // ABSL_HAVE_ADDRESS_SANITIZER
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
