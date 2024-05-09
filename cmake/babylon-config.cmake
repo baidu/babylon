2d846616186d8d8d9db2b2a77aabb9cb4202942e
Kevin Li <oathdruid@live.cn>
2024-05-09 14:42:47 +0800

    add cmake

--- cmake/babylon-config.cmake
+++ cmake/babylon-config.cmake
@@ -1,7 +1,5 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(absl REQUIRED)
-find_dependency(protobuf REQUIRED)
-find_dependency(Boost REQUIRED)
+include(${CMAKE_CURRENT_LIST_DIR}/babylon-deps.cmake)
 
 include(${CMAKE_CURRENT_LIST_DIR}/babylon-targets.cmake)
