a4973da045e431b4f46b1937123cfc452c0ab9a4
Kevin Li <oathdruid@live.cn>
2024-05-17 12:27:57 +0800

    add aarch64 CI pipeline

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,6 +3,7 @@ set -ex
 
 find bazel-out/k8-fastbuild/bin/test/ -name test_* | fgrep -v . | fgrep -v _objs | while read file; do
   if readelf -d $file | fgrep ld-linux-aarch64.so.1; then
+    ASAN_OPTIONS=detect_leaks=0 \
     qemu-aarch64 -L /usr/aarch64-linux-gnu $file
   else
     $file
