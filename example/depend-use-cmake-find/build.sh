1c73ae4f358d90cec9b83357b6f6247fef26a339
Kevin Li <oathdruid@live.cn>
2024-06-18 21:23:12 +0800

    upgrade example depend to 1.2.0

--- example/depend-use-cmake-find/build.sh
+++ example/depend-use-cmake-find/build.sh
@@ -5,9 +5,9 @@ set -ex
 # sudo apt instal boost
 # sudo apt instal protobuf
 
-URL=https://github.com/baidu/babylon/archive/refs/tags/v1.1.6.tar.gz
-NAME=babylon-1.1.6
-SHA256=a5bbc29f55819c90e00b40f9b5d2716d5f0232a158d69c530d8c7bac5bd794b6
+URL=https://github.com/baidu/babylon/archive/refs/tags/v1.2.0.tar.gz
+NAME=babylon-1.2.0
+SHA256=7e83e5177e44e98c48373a751eff6d90367cad0d539641ed4b59558606eed727
 if ! echo "$SHA256 $NAME.tar.gz" | sha256sum -c; then
   wget $URL --continue -O $NAME.tar.gz
 fi
