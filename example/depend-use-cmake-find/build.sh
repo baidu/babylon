f6cff03e7417cf3411c0587deb63bd5c46023ae7
Kevin Li <oathdruid@live.cn>
2024-11-07 23:11:30 +0800

    fix example

--- example/depend-use-cmake-find/build.sh
+++ example/depend-use-cmake-find/build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 URL=https://github.com/baidu/babylon/releases/download/v1.4.2/v1.4.2.tar.gz
-NAME=babylon-1.4.1
+NAME=babylon-1.4.2
 SHA256=d60ee9cd86a777137bf021c8861e97438a69cc857659d5eb39af9e8464434cf1
 if ! echo "$SHA256 $NAME.tar.gz" | sha256sum -c; then
   wget $URL --continue -O $NAME.tar.gz
